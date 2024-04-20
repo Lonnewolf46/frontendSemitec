85b4d1239bb768054b84324798743eae0cd65571
Vega-Luis <lanvega76r@gmail.com>
2024-04-20 08:45:11 -0600

    feat: adding demo key color changing on keydown

--- semitec/src/app/components/key.js
+++ semitec/src/app/components/key.js
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
 import styles from "./Keyboard.module.css";
-export default function Key({value, color}) {
-    return (
-        <div className={styles.key} style={{backgroundColor: color}}>
-            <span>{value}</span>
-        </div>
-    )
+
+export default function Key({ value, color }) {
+  const [keyColor, setKeyColor] = useState(color);
+  const [fontColor, setFontColor] = useState("#000000");
+
+  const handleKeyDown = (event) => {
+    if (value === event.key) {
+      setKeyColor("#007172");
+      setFontColor("#ffffff");
+    }
+  };
+
+  const handleKeyUp = (event) => {
+    if (value === event.key) {
+      setKeyColor(color);
+      setFontColor("#000000");
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+  }, []);
+  return (
+    <div className={styles.key} style={{ backgroundColor: keyColor }}>
+      <span style={{ color: fontColor }}>{value}</span>
+    </div>
+  );
 }
