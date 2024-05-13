bb12e0fe5149392b29ea7fbcd170d95ba4d5f27e
Valeria Sell <70655662+valeria-sell@users.noreply.github.com>
2024-05-13 11:49:10 -0600

    added icon image

--- semitec/src/app/components/profile-data.js
+++ semitec/src/app/components/profile-data.js
@@ -1,30 +1,36 @@
 import "./profile-data.css"
+import avatar from "@/app/ui/avatar.svg"
 
 export default function Profile({username,institution,user_code,user_type,email,country,province,canton,district}) {
     return (
-        <div className="flex-container">
-            <div className={"data-container"}> 
-                <div className="header-format">
-                    <div className="name-format">
-                        {username}
+        <div>
+            <div className="icon">
+                
+            </div>
+            <div className="flex-container">
+                <div className={"data-container"}> 
+                    <div className="header-format">
+                        <div className="name-format">
+                            {username}
+                        </div>
+                        <div className="institution-format">
+                            {institution}
+                        </div>
+                        <div className="id-format">
+                            #{user_code}
+                        </div>
                     </div>
-                    <div className="institution-format">
-                        {institution}
-                    </div>
-                    <div className="id-format">
-                        #{user_code}
-                    </div>
-                </div>
-            
-                <div className="body-format">
-                    <div className="data">
-                        {user_type}
-                    </div>
-                    <div className="data">
-                        {email}
-                    </div>
-                    <div className="data">
-                        {country}, {province}, {canton}, {district}
+                
+                    <div className="body-format">
+                        <div className="data">
+                            {user_type}
+                        </div>
+                        <div className="data">
+                            {email}
+                        </div>
+                        <div className="data">
+                            {country}, {province}, {canton}, {district}
+                        </div>
                     </div>
                 </div>
             </div>
