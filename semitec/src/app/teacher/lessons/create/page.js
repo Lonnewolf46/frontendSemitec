5d54043cb2fad79034f4646db0d3ed072bdab9d3
Valeria Sell <70655662+valeria-sell@users.noreply.github.com>
2024-05-21 05:22:33 -0600

    fix create lesson
    post method fixed

--- semitec/src/app/teacher/lessons/create/page.js
+++ semitec/src/app/teacher/lessons/create/page.js
@@ -6,16 +6,17 @@ import styles from "@/app/_styles/CreateLesson.module.css";
 import buttonStyles from "@/app/_styles/Button.module.css";
 export default function CreatLesson() {
   const router = useRouter();
-  const [var_name, setVarName] = useState("");
-  const [var_level_id, setLevelID] = useState("");
-  const [var_words, setVarWords] = useState("");
-  const [var_description, setDescription] = useState("");
-  const [var_min_time, setMinTime] = useState("");
-  const [var_min_mistakes, setMinMistakes] = useState("");
+  const [name, setVarName] = useState("");
+  const [level_id, setLevelID] = useState("");
+  const [words, setVarWords] = useState("");
+  const [description, setDescription] = useState("");
+  const [min_time, setMinTime] = useState("");
+  const [min_mistakes, setMinMistakes] = useState("");
   const [success, setSucess] = useState("");
 
   const createLesson = async () => {
     try {
+      console.log(JSON.stringify({ name : name, level_id : level_id, words : words, description : description, min_time : min_time, min_mistakes : min_mistakes }));
       const response = await fetch(
         "http://25.37.76.172:5000/teacher/lessons/create",
         {
@@ -24,13 +25,13 @@ export default function CreatLesson() {
             "Content-Type": "application/json",
             "auth-token": localStorage.getItem("auth-token"),
           },
-          body: JSON.stringify({ var_name : var_name, var_level_id : var_level_id, var_words : var_words, var_description : var_description, var_min_time : var_min_time, var_min_mistakes : var_min_mistakes }), 
+          body: JSON.stringify({ name : name, level_id : level_id, words : words, description : description, min_time : min_time, min_mistakes : min_mistakes }), 
         }
       );
       const data = await response.json();
       console.log(data);
       if (response.ok) {
-        success = true;
+        router.push("/teacher/lessons");
       }
     } catch (error) {
       console.log(error);
@@ -58,8 +59,7 @@ export default function CreatLesson() {
   
   const handleClick = () => {
     createLesson();
-    console.log(JSON.stringify({ var_name : var_name, var_level_id : var_level_id, var_words : var_words, var_description : var_description, var_min_time : var_min_time, var_min_mistakes : var_min_mistakes }));
-  };
+     };
 
   return (
     <div className={styles.container}>
@@ -73,12 +73,12 @@ export default function CreatLesson() {
             Nombre
           </label>
           <input style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
-            value={var_name}
+            value={name}
             minLength={1}
             maxLength={16}
             onChange={handleChangeName}
             type="text"
-            id="var_name"
+            id="name"
           />
           <label
             style={{ display: "block", marginBottom: "0.5em" }}
@@ -87,13 +87,13 @@ export default function CreatLesson() {
             Nivel
           </label>
           <input style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
-            value={var_level_id}
+            value={level_id}
             minLength={1}
             maxLength={16}
             onChange={handleChangeLevel}
             type="number" 
             required min="1"
-            id="var_level_id"
+            id="level_id"
           />
           <label
             style={{ display: "block", marginBottom: "0.5em" }}
@@ -102,13 +102,13 @@ export default function CreatLesson() {
             Texto
           </label>
           <textarea style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
-            value={var_words}
+            value={words}
             rows="5" 
             minLength={1}
             maxLength={1000}
             onChange={handleChangeWords}
             type="text"
-            id="var_words"
+            id="words"
           />
           <label
             style={{ display: "block", marginBottom: "0.5em" }}
@@ -117,13 +117,13 @@ export default function CreatLesson() {
             Descripción
           </label>
           <textarea style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
-            value={var_description}
+            value={description}
             rows="5" 
             minLength={1}
             maxLength={1000}
             onChange={handleChangeDescription}
             type="text"
-            id="var_description"
+            id="description"
           />
           <label
             style={{ display: "block", marginBottom: "0.5em" }}
@@ -132,12 +132,12 @@ export default function CreatLesson() {
             Límite de tiempo en segundos
           </label>
           <input style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
-            value={var_min_time}
+            value={min_time}
             minLength={1}
             maxLength={16}
             onChange={handleChangeMaxTime}
             type="text"
-            id="var_min_time"
+            id="min_time"
           />
           <label
             style={{ display: "block", marginBottom: "0.5em" }}
@@ -146,12 +146,12 @@ export default function CreatLesson() {
             Cantidad máxima de errores
           </label>
           <input style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
-            value={var_min_mistakes}
+            value={min_mistakes}
             minLength={1}
             maxLength={16}
             onChange={handleChangeMaxMistakes}
             type="text"
-            id="var_min_mistakes"
+            id="min_mistakes"
           />
         </div>
         {!success ? (
