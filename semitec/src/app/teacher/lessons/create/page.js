f1e17b9cc32c4871951799afd06bbcb43f5d089e
Vega-Luis <lanvega76r@gmail.com>
2024-06-03 08:32:51 -0600

    fix: fixing create lesson required inputs

--- semitec/src/app/teacher/lessons/create/page.js
+++ semitec/src/app/teacher/lessons/create/page.js
@@ -16,7 +16,16 @@ export default function CreatLesson() {
 
   const createLesson = async () => {
     try {
-      console.log(JSON.stringify({ name : name, level_id : level_id, words : words, description : description, min_time : min_time, min_mistakes : min_mistakes }));
+      console.log(
+        JSON.stringify({
+          name: name,
+          level_id: level_id,
+          words: words,
+          description: description,
+          min_time: min_time,
+          min_mistakes: min_mistakes,
+        })
+      );
       const response = await fetch(
         "http://25.37.76.172:5000/teacher/lessons/create",
         {
@@ -25,7 +34,14 @@ export default function CreatLesson() {
             "Content-Type": "application/json",
             "auth-token": localStorage.getItem("auth-token"),
           },
-          body: JSON.stringify({ name : name, level_id : level_id, words : words, description : description, min_time : min_time, min_mistakes : min_mistakes }), 
+          body: JSON.stringify({
+            name: name,
+            level_id: level_id,
+            words: words,
+            description: description,
+            min_time: min_time,
+            min_mistakes: min_mistakes,
+          }),
         }
       );
       const data = await response.json();
@@ -56,23 +72,35 @@ export default function CreatLesson() {
   const handleChangeMaxMistakes = (event) => {
     setMinMistakes(event.target.value);
   };
-  
-  const handleClick = () => {
+
+  const handleClick = (event) => {
+    event.preventDefault();
     createLesson();
-     };
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <h1 className={styles.title}>Crear Lección</h1>
-        <div style={{ marginBottom: "15px", textAlign: "left", fontSize: "1.3vw", }}>
+        <form
+          onSubmit={handleClick}
+          style={{ marginBottom: "15px", textAlign: "left", fontSize: "1.3vw" }}
+        >
           <label
             style={{ display: "block", marginBottom: "0.5em" }}
-            htmlFor="correo"
+            htmlFor="name"
           >
             Nombre
           </label>
-          <input style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
+          <input
+            style={{
+              width: "100%",
+              fontSize: "1.3vw",
+              borderRadius: "4px",
+              border: "solid 1px #ebebeb",
+              padding: "0.3em",
+            }}
+            required
             value={name}
             minLength={1}
             maxLength={16}
@@ -81,30 +109,57 @@ export default function CreatLesson() {
             id="name"
           />
           <label
-            style={{ display: "block", marginBottom: "0.5em" }}
-            htmlFor="correo"
+            style={{
+              display: "block",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            }}
+            htmlFor="level_id"
           >
             Nivel
           </label>
 
-          <input name="level_id" list="level_id" style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}/>
-              <datalist id="level_id">
-                <option value="1"></option>
-                <option value="2"></option>
-                <option value="3"></option>
-                <option value="4"></option>
-              </datalist>
-          
+          <select
+            id="level_id"
+            style={{
+              width: "100%",
+              fontSize: "1.3vw",
+              borderRadius: "4px",
+              border: "solid 1px #ebebeb",
+
+              padding: "0.3em",
+            }}
+            onChange={(e) => {
+              setLevelID(e.target.value);
+            }}
+          >
+            <option value="1">Nivel 1</option>
+            <option value="2">Nivel 2</option>
+            <option value="3">Nivel 3</option>
+            <option value="4">Nivel 4</option>
+          </select>
 
           <label
-            style={{ display: "block", marginBottom: "0.5em" }}
-            htmlFor="correo"
+            style={{
+              display: "block",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            }}
+            htmlFor="words"
           >
-            Texto
+            Contenido de la Lección
           </label>
-          <textarea style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
+          <textarea
+            required
+            style={{
+              width: "100%",
+              fontSize: "1.3vw",
+              borderRadius: "4px",
+              border: "solid 1px #ebebeb",
+              padding: "0.3em",
+            }}
             value={words}
-            rows="5" 
+            rows="5"
             minLength={1}
             maxLength={1000}
             onChange={handleChangeWords}
@@ -112,14 +167,27 @@ export default function CreatLesson() {
             id="words"
           />
           <label
-            style={{ display: "block", marginBottom: "0.5em" }}
-            htmlFor="correo"
+            style={{
+              display: "block",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            }}
+            htmlFor="description"
           >
             Descripción
           </label>
-          <textarea style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
+          <textarea
+            required
+            style={{
+              width: "100%",
+              fontSize: "1.3vw",
+              borderRadius: "4px",
+              border: "solid 1px #ebebeb",
+
+              padding: "0.3em",
+            }}
             value={description}
-            rows="5" 
+            rows="5"
             minLength={1}
             maxLength={1000}
             onChange={handleChangeDescription}
@@ -127,12 +195,24 @@ export default function CreatLesson() {
             id="description"
           />
           <label
-            style={{ display: "block", marginBottom: "0.5em" }}
-            htmlFor="correo"
+            style={{
+              display: "block",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            }}
+            htmlFor="min_time"
           >
             Límite de tiempo en segundos
           </label>
-          <input style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
+          <input
+            required
+            style={{
+              width: "100%",
+              fontSize: "1.3vw",
+              borderRadius: "4px",
+              border: "solid 1px #ebebeb",
+              padding: "0.3em",
+            }}
             value={min_time}
             minLength={1}
             maxLength={8}
@@ -141,12 +221,24 @@ export default function CreatLesson() {
             id="min_time"
           />
           <label
-            style={{ display: "block", marginBottom: "0.5em" }}
-            htmlFor="correo"
+            style={{
+              display: "block",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            }}
+            htmlFor="min_mistakes"
           >
             Cantidad máxima de errores
           </label>
-          <input style={{width: "100%", fontSize: "1.3vw", borderRadius: "4px", border: "solid 1px #ebebeb", padding: "0.3em" }}
+          <input
+            required
+            style={{
+              width: "100%",
+              fontSize: "1.3vw",
+              borderRadius: "4px",
+              border: "solid 1px #ebebeb",
+              padding: "0.3em",
+            }}
             value={min_mistakes}
             minLength={1}
             maxLength={8}
@@ -154,20 +246,16 @@ export default function CreatLesson() {
             type="number"
             id="min_mistakes"
           />
-        </div>
-        {!success ? (
-          <button onClick={handleClick} className={buttonStyles.primary}>
-            Crear
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              router.push("/teacher/lessons");
-            }}
-          >
-            Continuar
-          </button>
-        )}
+          <div style={{ margin: "auto", width: "fit-content" }}>
+            <button
+              style={{ marginTop: "1em" }}
+              type="submit"
+              className={buttonStyles.primary}
+            >
+              Crear
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
