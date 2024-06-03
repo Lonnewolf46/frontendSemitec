c80a634106e763034a0d69905d98d666d2be44d1
Vega-Luis <lanvega76r@gmail.com>
2024-06-03 06:29:58 -0600

    fix: fixing groups teacher groups structure

--- semitec/src/app/teacher/groups/page.js
+++ semitec/src/app/teacher/groups/page.js
@@ -1,24 +1,26 @@
-"use client"
-import { usePathname, useRouter } from 'next/navigation';
-import TeacherGroupsTable from '../../components/teacher-groups-table';
-import buttons from '@/app/_styles/Button.module.css'
-import containers from '@/app/_styles/Containers.module.css'
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import TeacherGroupsTable from "../../components/teacher-groups-table";
+import buttons from "@/app/_styles/Button.module.css";
+import containers from "@/app/_styles/Containers.module.css";
+import styles from "@/app/_styles/TeacherGroups.module.css";
 
 export default function TeacherGroups() {
   const router = useRouter();
   const pathname = usePathname();
 
   const handleClick = () => {
-    router.push(`${pathname}/create`)
-    
-  }
+    router.push(`${pathname}/create`);
+  };
   return (
-    <main style={{ width: "90vw", margin: "30px auto ", height: "70vh"}}>
-      <h1 style={{ fontSize: "2.7vw"}}>Mis grupos</h1>
-      <TeacherGroupsTable />
-      <div className={containers.alignLeft}>
-        <button className={buttons.primary} onClick={ handleClick }>Nuevo Grupo</button>
+    <main className={styles.mainContainer}>
+      <div className={styles.flexContainer}>
+        <h1 style={{ fontSize: "2.7vw" }}>Mis grupos</h1>
+        <button className={buttons.primary} onClick={handleClick}>
+          Nuevo Grupo
+        </button>
       </div>
+      <TeacherGroupsTable />
     </main>
   );
 }
