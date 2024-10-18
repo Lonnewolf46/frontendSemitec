404843ffad0b597ee58677cb6c161131cfd4c4f3
Lonnewolf46 <alekeicg@gmail.com>
2024-10-17 18:35:13 -0600

    Updated layout of Guest, Teacher and Student to incorporate dropdown Activities menu. Keyboard functionality to 100%. NVDA compatibility checked

--- semitec/src/app/guest/layout.js
+++ semitec/src/app/guest/layout.js
@@ -2,9 +2,9 @@ import { AccessibilityBar } from "../components/accessibility-bar";
 import NavBar from "../components/NavBar";
 
 const menuList = [
-  { text: "Lecciones", href: "/guest/lessons" },
-  { text: "Acerca de", href: "/guest/about" },
-  { text: "Preguntas frecuentes", href: "/guest/faqs" },
+  { text: "Lecciones", href: "/guest/lessons", children: [{text: "Predeterminadas", href: "/teacher/lessons"}, {text: "Públicas", href: "/teacher/lessons"}]  },
+  { text: "Acerca de", href: "/guest/about", children: [] },
+  { text: "Preguntas frecuentes", href: "/guest/faqs", children: [] },
 ];
 
 export default function GuestLayout({ children }) {
--- semitec/src/app/student/layout.js
+++ semitec/src/app/student/layout.js
@@ -2,11 +2,11 @@ import { AccessibilityBar } from "../components/accessibility-bar";
 import NavBar from "../components/NavBar";
 
 const menuList = [
-  { text: "Inicio", href: "/student/home" },
-  { text: "Mis grupos", href: "/student/groups" },
-  { text: "Lecciones", href: "/student/lessons" },
-  { text: "Acerca de", href: "/student/about" },
-  { text: "Preguntas frecuentes", href: "/student/faqs" },
+  { text: "Inicio", href: "/student/home", children: []  },
+  { text: "Mis grupos", href: "/student/groups", children: []  },
+  { text: "Actividades", href: "/student/lessons", children: [{text: "Tareas", href: "/teacher/lessons"}, {text: "Predeterminadas", href: "/teacher/lessons"}, {text: "Públicas", href: "/teacher/lessons"}]  },
+  { text: "Acerca de", href: "/student/about", children: []  },
+  { text: "Preguntas frecuentes", href: "/student/faqs", children: []  },
 ];
 
 export default function StudentLayout({
