d99fdfbd378ac3cb7fef0dcae7e3fd8861e42fb0
Vega-Luis <lanvega76r@gmail.com>
2024-11-08 14:53:42 -0600

    feat: adding teacher dashboard structure

--- semitec/src/app/teacher/home/page.js
+++ semitec/src/app/teacher/home/page.js
@@ -1,22 +1,26 @@
 "use client";
 import { useEffect, useState } from "react";
-import TeacherGroupsTable from "@/app/components/teacher-groups";
-import styles from "../../components/LessonsScreen.module.css";
+import styles from "./styles.module.css";
 import WelcomeCard from "@/app/components/welcome-card";
+import ActivityManagementCard from "@/app/components/dashboard-cards/activity-management-card";
+import RecentActivityCard from "@/app/components/dashboard-cards/recent-activity-card";
 
 export default function TeacherHome() {
   const [username, setUsername] = useState("");
   const getUsername = async () => {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/teacher/username`, {
-        headers: {
-          "auth-token": localStorage.getItem("auth-token"),
-        },
-      });
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_HOST}/teacher/username`,
+        {
+          headers: {
+            "auth-token": localStorage.getItem("auth-token"),
+          },
+        }
+      );
       const data = await res.json();
       if (res.ok) {
         console.log(data);
-        setUsername((data.username).split(" ")[0]);
+        setUsername(data.username.split(" ")[0]);
       }
     } catch (error) {
       console.log(error);
@@ -28,14 +32,44 @@ export default function TeacherHome() {
   }, []);
 
   return (
-    <main>
-      <div className={styles.lessonsMainContainer}>
-        <div className={styles.leftContainer}>
-          <WelcomeCard username={username} />
-        </div>
-        <section className={styles.rightContainer}>
-          <h1 style={{ fontSize: "2.7vw"}}>Mis grupos</h1>
-          <TeacherGroupsTable />
+    <main className={styles.mainWrapper}>
+      <section>
+        <WelcomeCard username={username} />
+      </section>
+      <div className={styles.column}>
+        <section className={styles.halfScreenContainer}>
+          <h1>Gestor de actividades</h1>
+          <div className={styles.cardWrapper}>
+            <ActivityManagementCard
+              text="Actividades creadas"
+              buttonText="Ver"
+            />
+            <ActivityManagementCard
+              text="Practicas predeterminadas"
+              buttonText="Ver"
+            />
+            <ActivityManagementCard
+              text="Actividades públicas"
+              buttonText="Ver"
+            />
+          </div>
+        </section>
+        <section className={styles.halfScreenContainer}>
+          <h1>Actividad reciente</h1>
+          <div className={styles.cardWrapper}>
+            <RecentActivityCard
+              title="Jesus ha completado: Tarea 1"
+              subtitle="8 de noviembre de 2024"
+            />
+            <RecentActivityCard
+              title="George ha completado: Tarea 3"
+              subtitle="27 de setiembre del 2024"
+            />
+            <RecentActivityCard
+              title="Heraldo ha completado tarea: 8"
+              subtitle="27 de setiembre del 2024"
+            />
+          </div>
         </section>
       </div>
     </main>
