1c4c53854955dc0e28f729873eaf5a47d0c6dea8
vsell <valeria_sell@outlook.com>
2024-11-14 12:34:16 -0600

    fix: dropdown bar now accesible

--- semitec/src/app/components/profileDropdown.jsx
+++ semitec/src/app/components/profileDropdown.jsx
@@ -1,26 +1,9 @@
 import { useState } from 'react';
-import Link from 'next/link';
-import Image from "next/image";
-import styles from './NavBar.module.css';
-import dropdownArrowIcon from "@/app/ui/arrow-drop-down.svg"
 
-const ProfileDropdown = ({ isOpen, openStateHandler, children }) => {
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter'){
-        event.preventDefault();
-        openStateHandler((prevState) => !prevState);
-      }
-  };
-
-  const handleBlur = (event) => {
-    if (!event.currentTarget.contains(event.relatedTarget)) {
-        openStateHandler(false);
-    }
-  };
+const ProfileDropdown = ({ isOpen, children }) => {
 
   return (
-    <div onBlur={handleBlur}>
+    <div>
       {isOpen && (  
         children
       )}
